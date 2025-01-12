0231624ab9c13a9b44acfc8bc8ce354b7c93b742
jasur <leo26292004@gmail.com>
2025-01-12 19:14:28 +0500

    home views html content update

--- olcha/views.py
+++ olcha/views.py
@@ -8,6 +8,10 @@ def home(request):
     # request: GET, POST, PUT, PATCH, DELETE
     html_content = """
     <h1>Assalomu aleykum</h1>
+    <h2>Mening ismim Jasur</h2>
+    <h3>Men Namangandanman, 2004 - yilman</h3>
+    <h4>Men TATU talabasiman</h4>
+    <h5>qiziqishlarim futbol, pls, tennis, kitob o'qish</h5>
     <p>Hurmatli do'stlar bu mening birinchi django web dasturim
     """
     return HttpResponse(html_content)
